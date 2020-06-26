5991ec73dd6f9c088d8f757de04a65f9522f876b
Yasshi-cookie <y.teruyacookie@gmail.com>
2020-06-26 14:25:04 +0900

    Create Customer model

---
+++ db/migrate/20200626045811_create_customers.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CreateCustomers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :customers do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end

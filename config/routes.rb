5fddde7229cd6ed30794bb103cc8f94d8886aab0
Yasshi-cookie <y.teruyacookie@gmail.com>
2020-07-31 20:16:40 +0900

    Add planner/logout path

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'home_pages/home'
   delete '/logout', to: 'sessions#destroy'
+  delete 'planner/logout', to: 'planner_sessions#destroy'
   resources :customers, only: %i[new create]
   resources :sessions, only: %i[new create]
   resources :planners, only: %i[new create]

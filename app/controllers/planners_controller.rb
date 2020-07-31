a66a99607c04f1207bfd108e440f014c57090fac
Yasshi-cookie <y.teruyacookie@gmail.com>
2020-07-31 11:49:19 +0900

    Create Planners controller

---
+++ app/controllers/planners_controller.rb
@@ -0,0 +1,7 @@
+class PlannersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'planners/new'
+  get 'planners/create'
   get 'sessions/new'
   root 'home_pages#home'
   get 'home_pages/home'

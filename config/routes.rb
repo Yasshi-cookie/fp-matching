95563d01403d782fef479c6522fc9ed032b51552
Yasshi-cookie <y.teruyacookie@gmail.com>
2020-08-11 15:37:15 +0900

    Rename SessionsHelper to CustomerSessionsHelper and Improve routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@
 Rails.application.routes.draw do
   root 'home_pages#home'
 
-  get 'sessions/new'
   get 'home_pages/home'
   delete '/logout', to: 'sessions#destroy'
   delete 'planner/logout', to: 'planner_sessions#destroy'

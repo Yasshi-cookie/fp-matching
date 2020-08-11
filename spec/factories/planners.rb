67eba91025dc5e3f219d5968dc9715d5ae1bb10b
Yasshi-cookie <y.teruyacookie@gmail.com>
2020-08-11 18:23:05 +0900

    Add test of FP signup and login and frozen string literal comment.

--- spec/factories/planners.rb
+++ spec/factories/planners.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :planner do
-    name { 'MyString' }
-    email { 'MyString' }
+    name { 'Example User' }
+    sequence(:email) { |n| "planner_#{n}@example.com" }
+    password { 'password' }
+    password_confirmation { 'password' }
   end
 end
--- spec/requests/planner_sessions_request_spec.rb
+++ spec/requests/planner_sessions_request_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'PlannerSessions', type: :request do

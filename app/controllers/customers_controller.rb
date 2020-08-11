8a9d67dd4f759de8afb994c00ab94d99a8232a89
Yasshi-cookie <y.teruyacookie@gmail.com>
2020-08-11 15:05:36 +0900

    remove unnecessary method and file and improve style

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,10 +5,6 @@ class CustomersController < ApplicationController
     @customer = Customer.new
   end
 
-  def show
-    @customer = Customer.find(params[:id])
-  end
-
   def create
     @customer = Customer.new(customer_params)
     if @customer.save

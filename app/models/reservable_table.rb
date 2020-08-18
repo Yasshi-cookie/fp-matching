291bbd010d289e25b98643b7d5b3baa081cba64e
Yasshi-cookie <y.teruyacookie@gmail.com>
2020-08-18 17:11:41 +0900

    Add registration of reservable datetime of Planner

--- app/models/reservable_table.rb
+++ app/models/reservable_table.rb
@@ -2,4 +2,21 @@ class ReservableTable < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :time_table
   belongs_to :planner
+
+  validates :date, presence: true
+  validates :time_table_id, presence: true, uniqueness: { scope: %i[date planner_id] }
+  validate :validate_that_time_table_is_available
+  validate :validate_that_reservable_date_is_before_today
+
+  private
+
+  def validate_that_time_table_is_available
+    return if date.nil? || time_table.nil?
+
+    errors.add(:time_table, 'は営業時間外です') unless time_table.available?(date)
+  end
+
+  def validate_that_reservable_date_is_before_today
+    errors.add(:date, ': 過去の日付は使用できません') if date.present? && date < Date.today
+  end
 end
--- app/models/time_table.rb
+++ app/models/time_table.rb
@@ -1,22 +1,37 @@
 class TimeTable < ActiveHash::Base
-  has_many :reservable_tables, dependent: :destroy
+  # has_many :reservable_tables, dependent: :destroy
 
   self.data = [
-    {id: 1, start_time: 10:00, end_time: 10:30},
-    {id: 2, start_time: 10:30, end_time: 11:00},
-    {id: 3, start_time: 11:00, end_time: 11:30},
-    {id: 4, start_time: 11:30, end_time: 12:00},
-    {id: 5, start_time: 12:00, end_time: 12:30},
-    {id: 6, start_time: 12:30, end_time: 13:00},
-    {id: 7, start_time: 13:00, end_time: 13:30},
-    {id: 8, start_time: 13:30, end_time: 14:00},
-    {id: 9, start_time: 14:00, end_time: 14:30},
-    {id: 10, start_time: 14:30, end_time: 15:00},
-    {id: 11, start_time: 15:00, end_time: 15:30},
-    {id: 12, start_time: 15:30, end_time: 16:00},
-    {id: 13, start_time: 16:00, end_time: 16:30},
-    {id: 14, start_time: 16:30, end_time: 17:00},
-    {id: 15, start_time: 17:00, end_time: 17:30},
-    {id: 16, start_time: 17:30, end_time: 18:00}
+    { id: 1, start_time: '10:00', end_time: '10:30' },
+    { id: 2, start_time: '10:30', end_time: '11:00' },
+    { id: 3, start_time: '11:00', end_time: '11:30' },
+    { id: 4, start_time: '11:30', end_time: '12:00' },
+    { id: 5, start_time: '12:00', end_time: '12:30' },
+    { id: 6, start_time: '12:30', end_time: '13:00' },
+    { id: 7, start_time: '13:00', end_time: '13:30' },
+    { id: 8, start_time: '13:30', end_time: '14:00' },
+    { id: 9, start_time: '14:00', end_time: '14:30' },
+    { id: 10, start_time: '14:30', end_time: '15:00' },
+    { id: 11, start_time: '15:00', end_time: '15:30' },
+    { id: 12, start_time: '15:30', end_time: '16:00' },
+    { id: 13, start_time: '16:00', end_time: '16:30' },
+    { id: 14, start_time: '16:30', end_time: '17:00' },
+    { id: 15, start_time: '17:00', end_time: '17:30' },
+    { id: 16, start_time: '17:30', end_time: '18:00' }
   ]
+
+  def time_range_text
+    "#{start_time} ~ #{end_time}"
+  end
+
+  def available?(date)
+    if date.saturday?
+      # 11時〜15時
+      id.between?(3, 10)
+    elsif date.sunday?
+      false
+    else
+      true
+    end
+  end
 end

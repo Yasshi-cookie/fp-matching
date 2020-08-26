d1e87fe2148a4d99847512d812f67d5ff1e72a42
Yasshi-cookie <y.teruyacookie@gmail.com>
2020-08-26 14:08:54 +0900

    Add reservation with planner

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,4 +1,20 @@
 class ReservationsController < ApplicationController
   def create
+    @reservation = current_customer
+                    .reservation
+                    .build(reservation_params)
+    if @reservation.save
+      flash[:success] = '面談を予約しました'
+      redirect_to customers_mypage_path
+    else
+      flash[:danger] = @reservation.errors.full_messages.join(',')
+      redirect_back fallback_location: root_path
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:reservable_table_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'reservations/create'
   root 'home_pages#home'
 
   get 'home_pages/home'
   delete '/logout', to: 'sessions#destroy'
   delete 'planner/logout', to: 'planner_sessions#destroy'
-  resources :customers, only: %i[new create]
-  resources :sessions, only: %i[new create]
-  resources :planners, only: %i[new create show index]
-  resources :planner_sessions, only: %i[new create]
-  resources :reservable_tables, only: %i[new create]
   namespace :customers do
     resource :mypage, only: %i[show]
   end
   namespace :planners do
     resource :mypage, only: %i[show]
   end
+  resources :customers, only: %i[new create]
+  resources :sessions, only: %i[new create]
+  resources :planners, only: %i[new create show index]
+  resources :planner_sessions, only: %i[new create]
+  resources :reservable_tables, only: %i[new create]
+  resources :reservations, only: %i[create]
 end
